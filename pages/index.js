98d307ed5f6bfe132cec0f23a1883d19e391df0e
Mike777p <mjp7@duck.com>
2023-06-15 17:52:47 -0500

    Add auth-o functionality

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import {useUser} from "@auth0/nextjs-auth0/client"
+
 export default function Home() {
+  const {user} = useUser();
+
+  console.log("User: ", user)
   return <div>
     <h1>This is my home page</h1>
+    <div>
+      {user ? 
+      <>
+        <div>User is logged in </div> 
+        <Link href="/api/auth/logout">Logout</Link> 
+      </> 
+      :
+        <Link href="/api/auth/login">Login</Link>
+
+      }
+    </div>
   </div>
 }
---
+++ pages/post/[postid].js
@@ -0,0 +1,6 @@
+export default function BlogPost() {
+    return <div>
+      <h1>This is my blog post page</h1>
+    </div>
+  }
+  
