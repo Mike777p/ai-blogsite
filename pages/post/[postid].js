1fc0ea208b59f540cd1f040a79a7a4d74ff1e109
Mike777p <mjp7@duck.com>
2023-06-15 18:29:41 -0500

    Add security to auth-o functionality

--- pages/post/[postid].js
+++ pages/post/[postid].js
@@ -1,6 +1,14 @@
+import { withPageAuthRequired } from "@auth0/nextjs-auth0"
+
 export default function BlogPost() {
     return <div>
       <h1>This is my blog post page</h1>
     </div>
   }
   
+
+  export const getServerSideProps = withPageAuthRequired(() => {
+    return {
+        props : {},
+    };  
+  });
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,6 +1,14 @@
-export default function NewPost() {
+import { withPageAuthRequired } from "@auth0/nextjs-auth0"
+
+export default function NewPost(props) {
+
     return <div>
       <h1>This is the new post page</h1>
     </div>
   }
   
+  export const getServerSideProps = withPageAuthRequired(() => {
+    return {
+        props : {},
+    };  
+  });
