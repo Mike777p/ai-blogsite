caf9fc1d658e5efb76df715cfcddfb8cf7b93eeb
Mike777p <mjp7@duck.com>
2023-06-16 18:22:19 -0500

    Add tailwing styling and layouts

--- pages/post/[postid].js
+++ pages/post/[postid].js
@@ -1,9 +1,14 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0"
+import { AppLayout } from "../../components/AppLayout";
 
 export default function BlogPost() {
     return <div>
       <h1>This is my blog post page</h1>
     </div>
+  }
+
+  BlogPost.getLayout = function getLayout(page, pageProbs) {
+    return <AppLayout {...pageProbs}>{page}</AppLayout>
   }
   
 
--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,10 +1,16 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0"
+import { AppLayout } from "../components/AppLayout";
 
 export default function TokenTopup() {
     return <div>
       <h1>This is my token topup page</h1>
     </div>
   }
+
+  TokenTopup.getLayout = function getLayout(page, pageProbs) {
+    return <AppLayout {...pageProbs}>{page}</AppLayout>
+  }
+  
   
   export const getServerSideProps = withPageAuthRequired(() => {
     return {
