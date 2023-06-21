1c26c9fafb58e535187a606245c6e863f411ce4c
Mike777p <mjp7@duck.com>
2023-06-21 18:11:34 -0500

    Add MongoDB functionality for user tokens

--- pages/token-topup.js
+++ pages/token-topup.js
@@ -2,8 +2,14 @@ import { withPageAuthRequired } from "@auth0/nextjs-auth0"
 import { AppLayout } from "../components/AppLayout";
 
 export default function TokenTopup() {
+  const handleClick = async () => {
+    await fetch("/api/addTokens", {
+      method : "POST",
+    })
+  }
     return <div>
       <h1>This is my token topup page</h1>
+      <button className="btn" onClick={handleClick}>Add tokens</button>
     </div>
   }
 
